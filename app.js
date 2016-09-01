b0ad20443b92960820b7a9d066fb21bc1348dd90
Jacks <191100479@qq.com>
2016-09-01 17:48:07 +0800

    add babel-polyfill

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 require('babel-register');
+//Babel默认只转换新的JavaScript句法（syntax），而不转换新的API，使用babel-polyfill扩展新的ES6 API
+require('babel-polyfill');
 require('./index.js');
-// require('./test.js');
+require('./test.js');
