e49582188f0624e8aae4fd7f7aed9ba4ceb906ba
Jacks <191100479@qq.com>
2016-10-21 21:51:43 +0800

    update

--- isomorphism/routes.js
+++ isomorphism/routes.js
@@ -4,14 +4,14 @@ import {Route} from 'react-router';
 // 无状态（stateless）组件，一个简单的容器，react-router 会根据 route
 // 规则匹配到的组件作为 `props.children` 传入
 const Container = (props) => {
-  return (
-    <div>{props.children}</div>
-  );
+    return (
+        <div>{props.children}</div>
+    );
 };
 
 export default(
-  <Route path="/" component={Container} >
-    <Route path="list" component={List} />
-    <Route path="item/:id" component={Item} />
-  </Route>
+    <Route path="/" component={Container}>
+        <Route path="list" component={List}/>
+        <Route path="item/:id" component={Item}/>
+    </Route>
 );
